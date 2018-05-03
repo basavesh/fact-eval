a73dd9a3b54adc632ec4fbd1131608929e538ecd
Sunjay Cauligi <scauligi@eng.ucsd.edu>
2018-05-03 14:50:50 -0700

    ssl3 bench imported complete

--- openssl/tests/openssl-fact/make_openssl.sh
+++ openssl/tests/openssl-fact/make_openssl.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-OPENSSL=$PWD
 OBJ_DIR=../..
 
 # copy FaCT port
-cp $OBJ_DIR/obj/s3_cbc.O2.h $OPENSSL/ssl/fact_s3_cbc.h
-cp $OBJ_DIR/obj/s3_cbc.O2.s $OPENSSL/ssl/_s3_cbc.s
+cp $OBJ_DIR/obj/s3_cbc.O2.h ssl/fact_s3_cbc.h
+cp $OBJ_DIR/obj/s3_cbc.O2.s ssl/_s3_cbc.s
 
 # (re)compile with fact stubbed in
-cd $OPENSSL
 touch ssl/s3_cbc.c
-cd $OPENSSL
+
+# configure and build
+CC=clang-6.0 CXX=clang++-6.0 ./config -static enable-ssl3 enable-ssl3-method
 make -j4
