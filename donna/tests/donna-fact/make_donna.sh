4881b718e242a986ae8a73215c67cb30c0cfee41
Sunjay Cauligi <scauligi@eng.ucsd.edu>
2018-05-03 14:27:10 -0700

    nits on donna-fact making

--- donna/tests/donna-fact/make_donna.sh
+++ donna/tests/donna-fact/make_donna.sh
@@ -5,7 +5,7 @@ DONNA=$PWD
 OBJ_DIR=../..
 
 # copy FaCT port
-cp $OBJ_DIR/obj/curve25519-c64.o $DONNA/curve25519-donna-fact64.o
+cp $OBJ_DIR/obj/curve25519-c64.O2.o $DONNA/curve25519-donna-fact64.o
 
 # (re)compile with fact stubbed in
 make test -j4
