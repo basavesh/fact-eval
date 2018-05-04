45624175426daecd8bfa120e924c731a3a376bdd
Sunjay Cauligi <scauligi@eng.ucsd.edu>
2018-05-03 22:54:35 -0700

    saga-fact-nice

--- saga/tests/openssl-fact-raw/make_openssl.sh
+++ saga/tests/openssl-fact-raw/make_openssl.sh
@@ -4,8 +4,8 @@ set -e
 OPENSSL=$PWD
 OBJ_DIR=../..
 
-cp $OBJ_DIR/obj/20110823_fast.h $OPENSSL/crypto/evp/fact_aes_cbc_hmac_sha1_decrypt.h
-cp $OBJ_DIR/obj/20110823_fast.s $OPENSSL/crypto/evp/e_aes_cbc_hmac_sha1_decrypt.s
+cp $OBJ_DIR/obj/20170717_latest_raw.h $OPENSSL/crypto/evp/fact_aes_cbc_hmac_sha1_decrypt.h
+cp $OBJ_DIR/obj/20170717_latest_raw.s $OPENSSL/crypto/evp/e_aes_cbc_hmac_sha1_decrypt.s
 
 CC=clang-6.0 CXX=clang++-6.0 ./config -static enable-ssl3 enable-ssl3-method
 make -j4
