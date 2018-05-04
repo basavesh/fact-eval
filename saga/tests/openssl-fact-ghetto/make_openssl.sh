ffb541fa07918785963ed60691b42bb9bfa92364
Sunjay Cauligi <scauligi@eng.ucsd.edu>
2018-05-03 22:52:40 -0700

    saga-fact-ghetto

---
+++ saga/tests/openssl-fact-ghetto/make_openssl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+OPENSSL=$PWD
+OBJ_DIR=../..
+
+cp $OBJ_DIR/obj/20170717_latest.h $OPENSSL/crypto/evp/fact_aes_cbc_hmac_sha1_decrypt.h
+cp $OBJ_DIR/obj/20170717_latest.s $OPENSSL/crypto/evp/e_aes_cbc_hmac_sha1_decrypt.s
+
+CC=clang-6.0 CXX=clang++-6.0 ./config -static enable-ssl3 enable-ssl3-method
+make -j4
