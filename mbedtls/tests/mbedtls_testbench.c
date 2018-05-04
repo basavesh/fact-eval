4b35d16e4d395db3738aa03916d8ec6af0c47857
kwantam <kwantam@gmail.com>
2018-05-03 19:34:07 -0700

    mbedtls: add code to record execution time

--- mbedtls/tests/mbedtls_testbench.c
+++ mbedtls/tests/mbedtls_testbench.c
@@ -1,6 +1,8 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/random.h>
+#include <time.h>
 
 #include <gmp.h>
 
@@ -12,6 +14,27 @@
 
 #define ciL    (sizeof(mbedtls_mpi_uint))       /* chars in limb  */
 #define biL    (ciL << 3)                       /* bits  in limb  */
+
+#define MEASURE_TIME(CODE, REC)                                                                         \
+    do {                                                                                                \
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t_start);                                              \
+        CODE;                                                                                           \
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t_stop);                                               \
+        long tmp = 1000000000 * (t_stop.tv_sec - t_start.tv_sec) + (t_stop.tv_nsec - t_start.tv_nsec);  \
+        REC.tot += tmp;                                                                                 \
+        if (tmp > REC.max) { REC.max = tmp; }                                                           \
+        if (tmp < REC.min) { REC.min = tmp; }                                                           \
+    } while (false);
+#define SHOW_TIME(NAME, REC)                                                                                    \
+    do {                                                                                                        \
+        printf("%27s  min: %9ld  avg: %9ld  max: %9ld\n", NAME, REC.min, REC.tot >> (2 * logiters), REC.max);   \
+    } while (false);
+
+typedef struct {
+    long tot;
+    long min;
+    long max;
+} timelog_t;
 
 int main(int argc, char **argv) {
     unsigned primebits = 0;
@@ -31,6 +54,12 @@ int main(int argc, char **argv) {
     }
     int niters = 1 << logiters;
 
+    // C is original impl, CTC is "const-time" C, F is FaCT
+    timelog_t timeC = { 0, (1L << 40), 0 };
+    timelog_t timeCTC = timeC;
+    timelog_t timeF = timeC;
+    struct timespec t_start;
+    struct timespec t_stop;
 
     mbedtls_mpi E, P, RR, X, Y1, Y2, Y3;
     mbedtls_mpi_init(&E);
@@ -62,17 +91,15 @@ int main(int argc, char **argv) {
         mpz_nextprime(p, p);
 
         // make sure all the mpis have enough room
-        {
-            unsigned nlimbs = mpz_sizeinbase(p, 2);
-            nlimbs = (nlimbs / biL) + ((nlimbs % biL) != 0);
-            mbedtls_mpi_grow(&P, nlimbs + 2);
-            mbedtls_mpi_grow(&E, nlimbs + 2);
-            mbedtls_mpi_free(&RR);
-            mbedtls_mpi_grow(&X, nlimbs + 2);
-            mbedtls_mpi_grow(&Y1, nlimbs + 2);
-            mbedtls_mpi_grow(&Y2, nlimbs + 2);
-            mbedtls_mpi_grow(&Y3, nlimbs + 2);
-        }
+        unsigned nlimbs = mpz_sizeinbase(p, 2);
+        nlimbs =  1 + (nlimbs / biL) + ((nlimbs % biL) != 0);
+        mbedtls_mpi_grow(&P, nlimbs);
+        mbedtls_mpi_grow(&E, nlimbs);
+        mbedtls_mpi_free(&RR);
+        mbedtls_mpi_grow(&X, nlimbs);
+        mbedtls_mpi_grow(&Y1, nlimbs);
+        mbedtls_mpi_grow(&Y2, nlimbs);
+        mbedtls_mpi_grow(&Y3, nlimbs);
 
         // export p to P
         memset(P.p, 0, ciL * P.n);
@@ -97,24 +124,49 @@ int main(int argc, char **argv) {
             E.s = 1;
             mpz_export(E.p, NULL, -1, ciL, 0, 0, e);
 
+            // run once untimed each time we change P, to set RR
+            if (inner == 0) {
+                memset(Y1.p, 0, ciL * Y1.n);
+                mbedtls_mpi_exp_mod(&Y1, &X, &E, &P, &RR);
+            }
+
             memset(Y1.p, 0, ciL * Y1.n);
-            mbedtls_mpi_exp_mod(&Y1, &X, &E, &P, &RR);
+            MEASURE_TIME(mbedtls_mpi_exp_mod(&Y1, &X, &E, &P, &RR), timeC);
             //mbedtls_mpi_write_file("Y1= ", &Y1, 16, NULL);
 
             memset(Y2.p, 0, ciL * Y2.n);
-            mbedtls_mpi_exp_mod_simple(&Y2, &X, &E, &P, &RR);
+            MEASURE_TIME(mbedtls_mpi_exp_mod_simple(&Y2, &X, &E, &P, &RR), timeCTC);
             //mbedtls_mpi_write_file("Y2= ", &Y2, 16, NULL);
 
             memset(Y3.p, 0, ciL * Y3.n);
-            fact_mpi_exp_mod(&Y3, &X, &E, &P, &RR);
+            MEASURE_TIME(fact_mpi_exp_mod(&Y3, &X, &E, &P, &RR), timeF);
             //mbedtls_mpi_write_file("Y3= ", &Y3, 16, NULL);
 
+            bool pass1 = true, pass2 = true;
+            for (unsigned i = 0; i < nlimbs + 1; i++) {
+                pass1 = pass1 && (Y2.p[i] == Y1.p[i]);
+                pass2 = pass2 && (Y3.p[i] == Y1.p[i]);
+            }
+            if (!pass1) {
+                printf("ERROR mbedtls_mpi_exp_mod_simple gave wrong answer\n");
+            }
+            if (!pass2) {
+                printf("ERROR fact_mpi_exp_mod gave wrong answer\n");
+            }
+
             /*
+            // for debugging, check that gmp agrees
             mpz_powm(y, x, e, p);
             //gmp_printf("y = %Zx\n\n", y);
             */
         }
     }
+
+    // print out stats
+    printf("Ran 2^%d iterations total. Average times:\n", 2 * logiters);
+    SHOW_TIME("mbedtls_mpi_exp_mod", timeC);
+    SHOW_TIME("mbedtls_mpi_exp_mod_simple", timeCTC);
+    SHOW_TIME("fact_mpi_exp_mod", timeF);
 
     gmp_randclear(rstate);
     mpz_clear(x);
