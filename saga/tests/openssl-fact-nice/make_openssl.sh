a5fd28a609c95da838c28f253755f23538cd9c53
Sunjay Cauligi <scauligi@eng.ucsd.edu>
2018-09-24 03:14:39 -0700

    using the correct files for saga fact impl

--- saga/tests/openssl-fact-nice/make_openssl.sh
+++ saga/tests/openssl-fact-nice/make_openssl.sh
@@ -5,7 +5,7 @@ OPENSSL=$PWD
 OBJ_DIR=../..
 
 cp $OBJ_DIR/obj/20110823_fast.h $OPENSSL/crypto/evp/fact_aes_cbc_hmac_sha1_decrypt.h
-cp $OBJ_DIR/obj/20110823_fast.s $OPENSSL/crypto/evp/e_aes_cbc_hmac_sha1_decrypt.s
+cp $OBJ_DIR/obj/20110823_fast.O3.s $OPENSSL/crypto/evp/e_aes_cbc_hmac_sha1_decrypt.s
 
 CC=clang-6.0 CXX=clang++-6.0 ./config -static enable-ssl3 enable-ssl3-method
 make -j4
--- saga/tests/openssl-fact-raw/make_openssl.sh
+++ saga/tests/openssl-fact-raw/make_openssl.sh
@@ -5,7 +5,7 @@ OPENSSL=$PWD
 OBJ_DIR=../..
 
 cp $OBJ_DIR/obj/20170717_latest_raw.h $OPENSSL/crypto/evp/fact_aes_cbc_hmac_sha1_decrypt.h
-cp $OBJ_DIR/obj/20170717_latest_raw.s $OPENSSL/crypto/evp/e_aes_cbc_hmac_sha1_decrypt.s
+cp $OBJ_DIR/obj/20170717_latest_raw.O3.s $OPENSSL/crypto/evp/e_aes_cbc_hmac_sha1_decrypt.s
 
 CC=clang-6.0 CXX=clang++-6.0 ./config -static enable-ssl3 enable-ssl3-method
 make -j4
