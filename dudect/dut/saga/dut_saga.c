ea27595d0222d741c5c981be36759e17c8d3c269
scauligi <scauligi@eng.ucsd.edu>
2018-11-12 16:02:49 +0100

    dudect better initialization

--- dudect/dut/saga/dut_saga.c
+++ dudect/dut/saga/dut_saga.c
@@ -14,33 +14,13 @@
 const size_t chunk_size = 256;
 const size_t number_measurements = 1e5; // per test
 
-// these constants (key32) taken from openssl speed
-static const unsigned char key32[32] = {
-    0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0,
-    0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0, 0x12,
-    0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0, 0x12, 0x34,
-    0x78, 0x9a, 0xbc, 0xde, 0xf0, 0x12, 0x34, 0x56
-};
-
-// these constants fixed from randomly generated values
-static const unsigned char iv[16] = {
-    0x0b, 0xb9, 0x20, 0x8d, 0xad, 0x72, 0x63, 0x13,
-    0x99, 0x7f, 0x97, 0x20, 0x5c, 0x31, 0x49, 0xc3
-};
-
-// these constants fixed from randomly generated values
-static uint8_t mackey[20] = {
-  0xd3, 0xfa, 0x36, 0xa8, 0x74, 0x98, 0xdf, 0xd5,
-  0xe9, 0x7c, 0x71, 0x55, 0x49, 0x39, 0x3c, 0x82,
-  0xc7, 0x0b, 0x21, 0x59
-};
+static unsigned char key32[32];
+static unsigned char iv[16];
+static uint8_t mackey[20];
 
 // first 9 bytes random (seq num + "type")
 // next two are TLS version, next two are message length
-static uint8_t aad[13] = {
-  0x2c, 0xfc, 0xe3, 0x6c, 0xd2, 0x6d, 0x33, 0x09, 0x3a,
-  0x03, 0x00, 0x00, 0x80
-};
+static uint8_t aad[13];
 
 static const EVP_CIPHER *evp_cipher = NULL;
 static EVP_CIPHER_CTX *ctx = NULL;
@@ -59,6 +39,15 @@ void init_dut(void) {
   EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_SET_MAC_KEY, 20, mackey);
   EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_TLS1_AAD,
       EVP_AEAD_TLS1_AAD_LEN, aad);
+
+  randombytes(key32, 32);
+  randombytes(iv, 16);
+  randombytes(mackey, 20);
+  randombytes(aad, 9);
+  aad[9]  = 0x03;
+  aad[10] = 0x00;
+  aad[11] = 0x00;
+  aad[12] = 0x80;
 }
 
 void prepare_inputs(uint8_t *input_data, uint8_t *classes) {
--- dudect/dut/secretbox/dut_secretbox_msg.c
+++ dudect/dut/secretbox/dut_secretbox_msg.c
@@ -9,17 +9,8 @@
 const size_t chunk_size = 163;
 const size_t number_measurements = 1e5; // per test
 
-// these constants (nonce and key) taken from libsodium's tests
-static unsigned char firstkey[32] = { 0x1b, 0x27, 0x55, 0x64, 0x73, 0xe9, 0x85,
-                                      0xd4, 0x62, 0xcd, 0x51, 0x19, 0x7a, 0x9a,
-                                      0x46, 0xc7, 0x60, 0x09, 0x54, 0x9e, 0xac,
-                                      0x64, 0x74, 0xf2, 0x06, 0xc4, 0xee, 0x08,
-                                      0x44, 0xf6, 0x83, 0x89 };
-
-static unsigned char nonce[24] = { 0x69, 0x69, 0x6e, 0xe9, 0x55, 0xb6,
-                                   0x2b, 0x73, 0xcd, 0x62, 0xbd, 0xa8,
-                                   0x75, 0xfc, 0x73, 0xd6, 0x82, 0x19,
-                                   0xe0, 0x03, 0x6b, 0x7a, 0x0b, 0x37 };
+static unsigned char firstkey[32];
+static unsigned char nonce[24];
 
 static unsigned char m[163];
 static unsigned char c[163];
@@ -30,7 +21,10 @@ uint8_t do_one_computation(uint8_t *data) {
   return 0;
 }
 
-void init_dut(void) {}
+void init_dut(void) {
+  randombytes(firstkey, 32);
+  randombytes(nonce, 24);
+}
 
 void prepare_inputs(uint8_t *input_data, uint8_t *classes) {
   randombytes(input_data, number_measurements * chunk_size);
