d32737d2a9fd53c5426eb49efa35cf39bdde198d
Brian Johannesmeyer <bjohanne@eng.ucsd.edu>
2018-11-11 23:53:38 -0800

    openssl ctverifing with script

--- openssl/s3_cbc_ctverifwrapper.c
+++ openssl/s3_cbc_ctverifwrapper.c
@@ -1,6 +1,15 @@
 #include "ctverif.h"
 #include <stdint.h>
-#include "s3_cbc.h"
+
+/*secret*/ int32_t __ssl3_cbc_digest_record(
+  /*secret*/ uint8_t __v1_md_state[216],
+  /*secret*/ uint8_t __v2_mac_out[64],
+  /*secret*/ uint8_t __v3_hmac_pad[128],
+  const /*secret*/ uint8_t __v4_header[],
+  /*public*/ uint64_t __v47___v4_header_len,
+  const /*secret*/ uint8_t __v5_data[],
+  /*public*/ uint64_t __v48___v5_data_len,
+  /*secret*/ uint64_t __v6_data_plus_mac_size);
 
 /*secret*/ int32_t __ssl3_cbc_digest_record_wrapper(
   /*secret*/ uint8_t __v1_md_state[216],
--- openssl/s3_cbc_stub.c
+++ openssl/s3_cbc_stub.c
@@ -1,8 +1,6 @@
 #include <stdint.h>
 
-//void _md_transform(int ctx_type, void* ctx, const unsigned char *block) {}
 void SHA1_Transform(uint8_t *ctx, uint8_t *input) {}
 
-//void _md_final_raw(int ctx_type, void* ctx, unsigned char *md_out) {}
 void _md_final_raw(uint8_t *ctx, uint8_t *input) {}
 
