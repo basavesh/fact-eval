32e6b03beb232455841889f0ff4c13dba011297f
Sunjay Cauligi <scauligi@eng.ucsd.edu>
2019-02-21 12:04:06 -0800

    forgot to check in updated bench scripts for saga

--- openssl/tests/openssl-fact/apps/bench.sh
+++ openssl/tests/openssl-fact/apps/bench.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-echo ./openssl s_server -quiet -ssl3 -cipher AES256-SHA \< ../../../../1gb
-echo time ./openssl s_client -quiet -ssl3 -cipher AES256-SHA \>/dev/null
+for x in $(seq 5); do
+  ./openssl s_server -ign_eof -quiet -ssl3 -cipher AES256-SHA -cert server.pem < ../../../../4gb &
+  serv=$!
+  sleep 0.5
+  time ./openssl s_client -ign_eof >/dev/null
+  kill $serv
+done
--- saga/tests/openssl-c/apps/bench.sh
+++ saga/tests/openssl-c/apps/bench.sh
@@ -4,5 +4,15 @@
 #date >> benchmarks.log
 #./openssl speed -evp aes-256-cbc-hmac-sha1 2>&1 >/dev/null | tee -a benchmarks.log
 
-echo ./openssl s_server -quiet -ssl3 -cipher AES256-SHA \< ../../../../1gb
-echo time ./openssl s_client -quiet -ssl3 -cipher AES256-SHA \>/dev/null
+if [[ $# < 1 ]]; then
+  echo "need arg" >&2
+  exit 1
+fi
+
+for x in $(seq 5); do
+  ./openssl s_server -ign_eof -quiet -cipher AES256-SHA -cert server.pem < ../../../../$1 &
+  serv=$!
+  sleep 0.5
+  time ./openssl s_client -ign_eof >/dev/null
+  kill $serv
+done
