0c63a7f8075e30384cdf55c833ddda071dc21044
basavesh <basavesh.shivakumar@gmail.com>
2022-03-20 23:22:59 +0100

    wip modify */parse_bench.py

--- donna/tests/parse_bench.py
+++ donna/tests/parse_bench.py
@@ -10,14 +10,13 @@ uname = sys.argv[2] if len(sys.argv) > 2 else None
 
 with open(fname) as f:
     lines = (line.strip() for line in f.readlines())
-    lines = (line for line in lines if line.endswith('us'))
+    lines = (line for line in lines if 'donna-fact' in line)
 
 times = [float(line.split(',')[1].replace('us', '')) for line in lines]
-assert(len(times) == 2 * 5)
+assert(len(times) == 1 * 5)
 
-c = times[0::2]
-fact = times[1::2]
+fact = times[0::2]
 
-print('benchmark', '% overhead of fact', sep='\t')
-print("donna", (median(fact) / median(c) - 1) * 100, sep='\t')
+print('benchmark', 'cpucycles', sep='\t')
+print("donna", median(fact) , sep='\t')
 print()
--- openssl-ssl3/tests/parse_bench.py
+++ openssl-ssl3/tests/parse_bench.py
@@ -12,17 +12,14 @@ with open(fname) as f:
     lines = (line.strip() for line in f.readlines() if line.startswith('user'))
 
 times = [float(line.split()[1].replace('0m', '').replace('s', '')) for line in lines]
-assert(len(times) == 2 * 4 * 5)
+assert(len(times) == 1 * 4 * 5)
 
-c_256m = times[5:10]
-c_1g = times[10:15]
-c_4g = times[15:20]
-fact_256m = times[25:30]
-fact_1g = times[30:35]
-fact_4g = times[35:40]
+fact_256m = times[5:10]
+fact_1g = times[10:15]
+fact_4g = times[15:20]
 
-print('benchmark', '% overhead of fact', sep='\t')
-print('openssl-ssl3 256 MB', (median(fact_256m) / median(c_256m) - 1) * 100, sep='\t')
-print('openssl-ssl3 1 GB', (median(fact_1g) / median(c_1g) - 1) * 100, sep='\t')
-print('openssl-ssl3 4 GB', (median(fact_4g) / median(c_4g) - 1) * 100, sep='\t')
+print('benchmark', 'cpucycles', sep='\t')
+print('openssl-ssl3 256 MB', median(fact_256m), sep='\t')
+print('openssl-ssl3 1 GB', median(fact_1g), sep='\t')
+print('openssl-ssl3 4 GB', median(fact_4g) , sep='\t')
 print()
