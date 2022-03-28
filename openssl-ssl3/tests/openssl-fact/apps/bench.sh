e6bbaeeab574e5bd9a15e8adb34086521f849c86
basavesh <basavesh.shivakumar@gmail.com>
2022-03-28 21:17:06 +0200

    fix benchmark to cpucycles for openssl-ssl3

--- openssl-ssl3/tests/openssl-fact/apps/bench.sh
+++ openssl-ssl3/tests/openssl-fact/apps/bench.sh
@@ -5,16 +5,11 @@ if [[ $# < 1 ]]; then
   exit 1
 fi
 
-echo >> benchmarks.log
-date >> benchmarks.log
-echo $1 >> benchmarks.log
-
-for x in $(seq 5); do
-  echo >>benchmarks.log
-  echo $x >>benchmarks.log
-  ./openssl s_server -ign_eof -quiet -ssl3 -cipher AES256-SHA -cert server.pem < ../../../../$1 &
-  serv=$!
-  sleep 0.5
-  { time ./openssl s_client -ign_eof >/dev/null 2>/dev/null ; } 2>> benchmarks.log
-  kill $serv
+rm -f benchmarks.log
+#./openssl s_server -ign_eof -quiet -ssl3 -cipher AES256-SHA -cert server.pem >/dev/null &
+#serv=$!
+#sleep 0.5
+for x in $(seq 100); do
+  ./openssl s_client -no_tls1_3  < ../../../../$1 >/dev/null 2>/dev/null
 done
+#kill $serv
--- openssl-ssl3/tests/parse_bench.py
+++ openssl-ssl3/tests/parse_bench.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-from statistics import median
+import numpy as np
 
 assert(len(sys.argv) > 1)
 
@@ -9,17 +9,15 @@ fname = sys.argv[1]
 uname = sys.argv[2] if len(sys.argv) > 2 else None
 
 with open(fname) as f:
-    lines = (line.strip() for line in f.readlines() if line.startswith('user'))
+    lines = (line.strip() for line in f.readlines())
 
-times = [float(line.split()[1].replace('0m', '').replace('s', '')) for line in lines]
-assert(len(times) == 1 * 4 * 5)
+cpucycles = [line for line in lines]
+cpucycles = [int(x) for x in cpucycles]
 
-fact_256m = times[5:10]
-fact_1g = times[10:15]
-fact_4g = times[15:20]
+assert(len(cpucycles) == 100)
 
-print('benchmark', 'cpucycles', sep='\t')
-print('openssl-ssl3 256 MB', median(fact_256m), sep='\t')
-print('openssl-ssl3 1 GB', median(fact_1g), sep='\t')
-print('openssl-ssl3 4 GB', median(fact_4g) , sep='\t')
+print('benchmark', 'cpucycles', sep='\t\t\t')
+print('openssl-ssl3 25 percentile', np.percentile(cpucycles, 25), sep='\t')
+print('openssl-ssl3 50 percentile', np.percentile(cpucycles, 50), sep='\t')
+print('openssl-ssl3 75 percentile', np.percentile(cpucycles, 75), sep='\t')
 print()
